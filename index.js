e0bdb512a3ee24a364ab37c70afc10ffbbd7a671
arihant518 <arihanttongya123@gmail.com>
2022-08-27 13:35:20 -0700

    final code

---
+++ index.js
@@ -0,0 +1,113 @@
+// document.addEventListener("keypress",function(event){
+//   makeSound(event.key);
+// });
+// for (var i = 0; i < document.querySelectorAll(".drum").length; i++) {
+//   document.querySelectorAll(".drum")[i].addEventListener("click",function (){
+//       var buttonKey=this.innerHTML;
+//       makeSound(buttonKey);
+//       addAnimation(buttonKey);
+//   });
+// }
+//
+// document.addEventListener("keypress",function(event) {
+//     makeSound(event.key);
+//     addAnimation(event.key);
+// });
+//
+// function makeSound(key){
+//   switch (key) {
+//     case "w":
+//             var tom1 = new Audio('sounds/tom-1.mp3');
+//             tom1.play();
+//       break;
+//       case "a":
+//                var tom2 = new Audio('sounds/tom-2.mp3');
+//                tom2.play();
+//         break;
+//         case "s":
+//                var tom3 = new Audio('sounds/tom-3.mp3');
+//                tom3.play();
+//           break;
+//           case "d":
+//                 var tom4 = new Audio('sounds/tom-4.mp3');
+//                 tom4.play();
+//             break;
+//             case "j":
+//                  var snare = new Audio('sounds/snare.mp3');
+//                  snare.play();
+//               break;
+//               case "k":
+//                    var kick = new Audio('sounds/kick-bass.mp3');
+//                    kick.play();
+//                 break;
+//                case "l":
+//                      var crash = new Audio('sounds/crash.mp3');
+//                      crash.play();
+//                   break;
+//     default:  console.log(buttonkey);
+//
+//   }
+// }
+//
+// function addAnimation(currentKey){
+//     document.querySelector("." + currentKey).classList.add("pressed");
+//     setTimeout(function(){
+//       document.querySelector("." + currentKey).classList.remove("pressed");
+//     },300)
+// }
+
+
+
+$("button").click(function(buttonKey){
+  var buttonKey=this.innerHTML;
+  makeSound(buttonKey);
+  addAnimation(buttonKey);
+$(document).keypress(function(event){
+    makeSound(event.key);
+    addAnimation(event.key);
+  });
+});
+
+
+
+function makeSound(key){
+  switch (key) {
+    case "w":
+            var tom1 = new Audio('sounds/tom-1.mp3');
+            tom1.play();
+      break;
+      case "a":
+               var tom2 = new Audio('sounds/tom-2.mp3');
+               tom2.play();
+        break;
+        case "s":
+               var tom3 = new Audio('sounds/tom-3.mp3');
+               tom3.play();
+          break;
+          case "d":
+                var tom4 = new Audio('sounds/tom-4.mp3');
+                tom4.play();
+            break;
+            case "j":
+                 var snare = new Audio('sounds/snare.mp3');
+                 snare.play();
+              break;
+              case "k":
+                   var kick = new Audio('sounds/kick-bass.mp3');
+                   kick.play();
+                break;
+               case "l":
+                     var crash = new Audio('sounds/crash.mp3');
+                     crash.play();
+                  break;
+    default:  console.log(buttonkey);
+
+  }
+}
+
+function addAnimation(currentKey){
+  $("."+ currentKey).addClass("pressed");
+  setTimeout(function(){
+    $("."+ currentKey).removeClass("pressed");
+  },300);
+}
